16def49a9a51425a0deb2ee609fed57d7859c2de
Manish Solanki <152526919+manishkumar0015@users.noreply.github.com>
2024-10-04 08:34:59 +0530

    Add files via upload

---
+++ slickslider/js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $('.center').slick({
+        dots: true,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear'
+    });
+});
